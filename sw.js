a265480abed01c9cb7efb9b73564fba55f0698ba
andrewsheung-netizen <andrewsheung@gmail.com>
2026-06-12 12:17:07 +0800

    fix PWA
    fix PWA

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *   - EmailJS CDN                         → Cache-first (rarely changes)
  */
 
-const CACHE_VERSION = 'panorama-v9';
+const CACHE_VERSION = 'panorama-v11';
 
 const APP_SHELL = [
   './',
