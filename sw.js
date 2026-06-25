681a277a1533725096d86126e0f477d2d28fbc5e
andrewsheung-netizen <andrewsheung@gmail.com>
2026-06-25 11:54:39 +0800

    google drive photo

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *   - EmailJS CDN                         → Cache-first (rarely changes)
  */
 
-const CACHE_VERSION = 'panorama-v22';
+const CACHE_VERSION = 'panorama-v23';
 
 const APP_SHELL = [
   './',
