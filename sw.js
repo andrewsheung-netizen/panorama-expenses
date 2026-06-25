ea3ebcf9cc6ebdb6dc6158cee96f381b6735d902
andrewsheung-netizen <andrewsheung@gmail.com>
2026-06-25 12:57:07 +0800

    drive fix

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *   - EmailJS CDN                         → Cache-first (rarely changes)
  */
 
-const CACHE_VERSION = 'panorama-v24';
+const CACHE_VERSION = 'panorama-v25';
 
 const APP_SHELL = [
   './',
