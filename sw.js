ad5dd4ef7b2192ea70ab50ee5161d8b0fc888cbf
andrewsheung-netizen <andrewsheung@gmail.com>
2026-07-16 11:07:49 +0800

    input calculator

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *   - EmailJS CDN                         → Cache-first (rarely changes)
  */
 
-const CACHE_VERSION = 'panorama-v29';
+const CACHE_VERSION = 'panorama-v30';
 
 const APP_SHELL = [
   './',
