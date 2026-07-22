06949ed247ea5b6c0779a30a988eeae05539d1ec
andrewsheung-netizen <andrewsheung@gmail.com>
2026-07-22 16:58:55 +0800

    asd
    asd

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *   - EmailJS CDN                         → Cache-first (rarely changes)
  */
 
-const CACHE_VERSION = 'panorama-v36';
+const CACHE_VERSION = 'panorama-v37';
 
 const APP_SHELL = [
   './',
