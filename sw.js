941483ce247a7de1fa9e509d3a46f9bb409c6998
andrewsheung-netizen <andrewsheung@gmail.com>
2026-09-16 11:03:22 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  *   - EmailJS CDN                         → Cache-first (rarely changes)
  */
 
-const CACHE_VERSION = 'panorama-v61';
+const CACHE_VERSION = 'panorama-v62';
 
 const APP_SHELL = [
   './',
